02d5045fb873366dac556d6f9cda9182d0413ee0
siddharth1609 <siddharthsonwane18@gmail.com>
2020-03-01 23:27:01 +0530

    new file

--- src/main/java/com/springcloudconfigclient/SpringCloudConfigClientApplication.java
+++ src/main/java/com/springcloudconfigclient/SpringCloudConfigClientApplication.java
@@ -8,6 +8,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 public class SpringCloudConfigClientApplication {
 
